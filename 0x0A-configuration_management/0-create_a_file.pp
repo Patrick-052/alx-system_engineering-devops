dc15e20f52a604cef0e8e18327d005494c6b2844
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-23 18:58:16 +0300

    creating a management puppet script

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # I mananage the file path, owmership and permission
 
 file {'/tmp/school':
-    path   => '/tmp/school',
-    mode   => '0744',
-    owner  => 'www-data',
-    group  => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet'
 }
