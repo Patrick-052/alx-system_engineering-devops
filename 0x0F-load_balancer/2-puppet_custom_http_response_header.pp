444a68a6b87cc7627aac3c1d6853301c81a6bd50
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-03 23:40:28 +0300

    configurations using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,9 @@
 # Creating custom headers for a Http server response
 
+package { 'nginx':
+  ensure  =>  installed,
+}
+
 file { '/etc/nginx/sites-available/default':
   ensure  =>  present,
   content =>  "
@@ -8,20 +12,11 @@ server {
     listen [::]:80 default_server;
     root /var/www/html;
     index index.html;
-    location /redirect_me {
-       return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-    error_page 404 /404.html;
-    location = /404.html{
-       internal;
-    }
-    location / {
-       # First attempt to serve request as file, then
-       # as directory, then fall back to displaying a 404.
-       try_files ${uri} ${uri}/ =404;
-       # Adding a custom header
-       add_header X-Served-By '194200-web-01';
-    }
+    add_header X-Served-By ${hostname};
 }
 ",
+}
+
+exec { 'run'
+  command => '/usr/sbin/service nginx restart',
 }
