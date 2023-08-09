41600fcd9ecf18c18034edcbdd6e455f9d9aa7f3
Patrick-052 <muiruripatrick052@gmail.com>
2023-08-09 20:43:43 +0000

    magic of strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,25 +1,10 @@
-# Tracing bugs using [strace] command and debugging them ensuring
-# apache2 runs smoothly when requested for a webpage
+# A puppet manuscript to replace a line in a file on a server
 
-# Ensuring apache is installed
-package {'apache2':
-  ensure  => installed,
-}
+$file_to_edit = '/var/www/html/wp-settings.php'
 
-# Creating the missing file
-file {'/var/www/html/index.html':
-  ensure  => present,
-  content => '<title>Holberton &#8211; Just another WordPress site</title>
-<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
-<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
-        <div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div>  </div>
-                            <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
-        <p>Yet another bug by a Holberton student</p>',
-}
+#replace line containing "phpp" with "php"
 
-# Restarting apache
-service {'apache2':
-  ensure  => running,
-  enable  => true,
-  require => Package['apache2'],
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
