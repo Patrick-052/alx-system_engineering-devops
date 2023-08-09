b362484e6f307a7d4043ee9a49ee3c99205968e9
Patrick-052 <muiruripatrick052@gmail.com>
2023-08-09 21:08:04 +0000

    Identified and Resolved Apache 500 Error using strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,10 @@
-# A puppet manuscript to replace a line in a file on a server
+# Using [strace] to trace bugs and fixing them for 
+# smooth running of apache2 
 
 $file_to_edit = '/var/www/html/wp-settings.php'
 
-#replace line containing "phpp" with "php"
-
+# replace line containing "phpp" with "php"
 exec { 'replace_line':
   command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
+  path    => ['/bin','/usr/bin'] 
 }
